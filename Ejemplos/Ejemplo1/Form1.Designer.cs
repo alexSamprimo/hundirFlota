75e68d582ccee0483e415ef2c21acf428d6e855f
tonit <tonit@DESKTOP-KIDHU1D>
2022-10-05 18:30:09 +0200

    Agregar archivos de proyecto.

---
+++ Ejemplos/Ejemplo1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Ejemplo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnApretar = new System.Windows.Forms.Button();
+            this.lbSaludo = new System.Windows.Forms.Label();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnApretar
+            // 
+            this.btnApretar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApretar.Location = new System.Drawing.Point(147, 53);
+            this.btnApretar.Name = "btnApretar";
+            this.btnApretar.Size = new System.Drawing.Size(113, 41);
+            this.btnApretar.TabIndex = 0;
+            this.btnApretar.Text = "Aprieta aquí";
+            this.btnApretar.UseVisualStyleBackColor = true;
+            this.btnApretar.Click += new System.EventHandler(this.btnApretar_Click);
+            // 
+            // lbSaludo
+            // 
+            this.lbSaludo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSaludo.Location = new System.Drawing.Point(142, 129);
+            this.lbSaludo.Name = "lbSaludo";
+            this.lbSaludo.Size = new System.Drawing.Size(129, 38);
+            this.lbSaludo.TabIndex = 1;
+            this.lbSaludo.Text = "Hola Mundo";
+            this.lbSaludo.Visible = false;
+            
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(147, 184);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(113, 39);
+            this.btnBorrar.TabIndex = 2;
+            this.btnBorrar.Text = "borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.lbSaludo);
+            this.Controls.Add(this.btnApretar);
+            this.Name = "Form1";
+            this.Text = "Ejemplo1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnApretar;
+        private System.Windows.Forms.Label lbSaludo;
+        private System.Windows.Forms.Button btnBorrar;
+    }
+}
+
---
+++ Ejercicios_Parte_3/ejercicio1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pulsa el boton para intercambiar el contenido";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TEXTO A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(524, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TEXTO B";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(316, 174);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "<- ->";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(87, 209);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(100, 20);
+            this.txtBox1.TabIndex = 4;
+            this.txtBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(506, 208);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(100, 20);
+            this.txtBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+    }
+}
+
